531647bee40274c624c5356f86aa77cdeb00837e
Glenn Hickey <hickey@soe.ucsc.edu>
2011-11-26 16:23:23 -0800

    fix some bugs so networkX passes the abstract tests.
    Change-Id: Ib78b2726111935c2e6256c81834e3c6eceaca5c9

--- py/networkX.py
+++ py/networkX.py
@@ -1,21 +1,172 @@
-"""Interface for writing into Sqg into NetworkX.
+"""Interface for converting between Sqg and NetworkX.
 """
 import networkx as NX
-from pysqg.arrayList import AbstractArrayList
+from pysqg.arrayList import AbstractArrayList, InMemoryArrayList
 from pysqg.sqg import Sqg
 
+# return a NetowrkX representation of the given SQG
 def networkxWrite(sqg):
     if isSQGDirected(sqg):
         nxGraph = NX.DiGraph()
     else:
-        nxGraph = NX.Graph()
+        nxGraph = NX.Graph()    
     nxSubgraphs = []
     for arrayListType, arrayList in sqg.getArrayLists().items():
         writeArrayListIntoNX(arrayList, nxGraph)
     for arrayListType, arrayList in sqg.getArrayLists().items():
         writeArrayListIntoNXSubgraphs(arrayList, nxGraph, nxSubgraphs)
+    nxGraph.graph['includes'] = sqg.getIncludes()
+    nxGraph.graph['sqgName'] = sqg.getName()
+    nxGraph.graph['parents'] = sqg.getParents()
+    nxGraph.graph['sharedVariables'] = sqg.getSharedVariables()
     return nxGraph, nxSubgraphs
+
+# returns an SQG representation of the given NetowrkX graphs
+# (and optional subgraphs).  
+def networkxRead(nxGraph, nxSubgraphList = []):
+    sqgName = None
+    includes = None
+    parents = None
+    sharedVariables = None
+    if 'includes' in nxGraph.graph:
+        includes = nxGraph.graph['includes']
+    if 'sqgName' in nxGraph.graph:
+        sqgName = nxGraph.graph['sqgName']
+    if 'sharedVariables' in nxGraph.graph:
+        sharedVariables = nxGraph.graph['sharedVariables']
+    if 'parents' in nxGraph.graph:
+        parents = nxGraph.graph['parents']
     
+    sqg = Sqg(includes=includes, name=sqgName, 
+              sharedVariables=sharedVariables, parents=parents)
+      
+    readNodesFromNX(nxGraph, sqg)
+    readEdgesFromNX(nxGraph, sqg)
+    readSubgraphsFromNX(nxGraph, nxSubgraphList, sqg)
+    return sqg
+
+
+# private general function
+
+def isSQGDirected(sqg):
+    for arrayListType, arrayList in sqg.getArrayLists().items():
+        if arrayList.inheritsFrom('directedEdge'):
+            return True
+    return False
+        
+def isNXGraphDirected(nxGraph):
+    return type(nxGraph) == NX.DiGraph or type(nxGraph) == NX.MultiDiGraph    
+
+
+
+# private reading functions
+
+def getSQGVariablesFromNXAttributes(nxAttrib, excludeList = []):
+    variables = []
+    for key, val in nxAttrib.items():
+        if key not in excludeList:
+            variables.append(str(key))
+            variables.append(str(type(val).__name__))
+    return variables
+
+def readNodesFromNX(nxGraph, sqg):
+    for node in nxGraph.nodes():
+        nodeType = 'node'
+        nodeAttrib = nxGraph.node[node]
+        nodeArrayDict = nodeAttrib.copy()
+        if 'type' in nodeAttrib:
+            nodeType = nodeAttrib['type']
+            del nodeArrayDict['type']
+        nodeArrayDict['nodeName'] = node
+        
+        nodeArrayList = sqg.getArrayList(nodeType)
+        if nodeArrayList is None:
+            variables = None
+            if nodeType not in AbstractArrayList._arrayListTypes:
+                variables = getSQGVariablesFromNXAttributes(nodeArrayDict)
+            nodeArrayList = InMemoryArrayList(type=nodeType, variables=variables)
+            sqg.setArrayList(nodeArrayList)
+        
+        nodeArrayList.addDict(nodeArrayDict) 
+
+def readEdgesFromNX(nxGraph, sqg):
+    for edge in nxGraph.edges():
+        edgeAttrib = nxGraph[edge[0]][edge[1]]
+        edgeArrayDict = edgeAttrib.copy()
+        if "directed" in edgeAttrib:
+            del edgeArrayDict['directed']
+            edgeArrayDict["outNode"] = edge[0]
+            edgeArrayDict["inNode"] = edge[1]
+            edgeType = 'directedEdge'
+        else:
+            edgeArrayDict["node1"] = edge[0]
+            edgeArrayDict["node2"] = edge[1]
+            edgeType = 'edge'
+
+        if 'type' in edgeAttrib:
+            edgeType = edgeAttrib['type']
+            del edgeArrayDict['type']
+                
+        edgeArrayList = sqg.getArrayList(edgeType)
+        if edgeArrayList is None:
+            variables = None
+            if edgeType not in AbstractArrayList._arrayListTypes:
+                variables = getSQGVariablesFromNXAttributes(edgeArrayDict)
+            edgeArrayList = InMemoryArrayList(type=edgeType, variables=variables)
+            sqg.setArrayList(edgeArrayList)
+        
+        edgeArrayList.addDict(edgeArrayDict)
+
+def readSubgraphsFromNX(nxGraph, nxSubgraphList, sqg):
+    for nxSubgraph in nxSubgraphList:
+        subgraphType = 'subgraph'
+        edgeTypes = None
+        subgraphAttrib = nxSubgraph.graph
+        subgraphArrayDict = subgraphAttrib.copy()
+        if 'type' in subgraphAttrib:
+            subgraphType = subgraphAttrib['type']
+            del subgraphArrayDict['type']
+        if 'edgeTypes' in subgraphAttrib:
+            edgeTypes = subgraphAttrib['edgeTypes']
+            del subgraphArrayDict['edgeTypes']
+        subgraphArrayDict['nodes'] = nxSubgraph.nodes()
+        
+        subgraphArrayList = sqg.getArrayList(subgraphType)
+        if subgraphArrayList is None:
+            variables, sharedVariables = None, None
+            if subgraphType not in AbstractArrayList._arrayListTypes:
+                variables = getSQGVariablesFromNXAttributes(subgraphArrayDict)
+                if edgeTypes is not None:
+                    sharedVariables = dict()
+                    sharedVariables["edges"] = edgeTypes
+            subgraphArrayList = InMemoryArrayList(type=subgraphType, 
+                                                  sharedVariables=sharedVariables, 
+                                                  variables=variables)
+            sqg.setArrayList(subgraphArrayList)
+        
+        subgraphArrayList.addDict(subgraphArrayDict)
+
+
+
+## private writing functions
+
+def writeArrayListIntoNX(arrayList, nxGraph):
+    if arrayList.inheritsFrom('node'):
+        writeNodesIntoNX(arrayList, nxGraph)
+    elif arrayList.inheritsFrom('edge'):
+        writeEdgesIntoNX(arrayList, nxGraph)
+    elif arrayList.inheritsFrom('directedEdge'):
+        writeDirectedEdgesIntoNX(arrayList, nxGraph)
+    elif arrayList.inheritsFrom('subgraph'):
+        pass
+    else:
+        writeMetadataIntoNX(arrayList, nxGraph)
+
+# to be called after writeArrayListIntoNX (ie nxGraph already filled)
+def writeArrayListIntoNXSubgraphs(arrayList, nxGraph, nxSubgraphs):
+    if arrayList.inheritsFrom('subgraph'):
+        writeSubgraphsIntoNX(arrayList, nxGraph, nxSubgraphs)
+
 def setNXAttributesFromArrayList(nxAttrib, arrayList, newArray, excludeList):
     names = arrayList.getArrayNames()
     nxAttrib["type"] = arrayList.getType()  
@@ -70,6 +221,7 @@ def writeDirectedEdgesIntoNX(edgeArrayList, nxGraph):
         nxGraph.add_edge(outNodeName, inNodeName)
         setNXAttributesFromArrayList(nxGraph[outNodeName][inNodeName], edgeArrayList, 
                                      edgeArray, [outNodeIndex, inNodeIndex])
+        nxGraph[outNodeName][inNodeName]["directed"] = 1
 
 def writeSubgraphsIntoNX(subgraphArrayList, nxGraph, nxSubgraphs):
     assert nxGraph is not None
@@ -78,55 +230,28 @@ def writeSubgraphsIntoNX(subgraphArrayList, nxGraph, nxSubgraphs):
     if 'subgraphName' not in subgraphArrayList.getArrayNames():
         raise RuntimeError("Could not find subgraphName in subgraph") 
     nodesIndex = subgraphArrayList.getArrayNames().index('nodes')
-    subgraphNameIndex = subgraphArrayList.getArrayNames().index('subgraphName')
     
     sharedVariables = subgraphArrayList.getSharedVariables()
     assert sharedVariables is not None
-    if 'edges' not in sharedVariables.getArrayNames():
+    if 'edges' not in sharedVariables:
         raise RuntimeError("Could not edges in subgraph's sharedVariables")
-    edgesIndex =  sharedVariables.getArrayNames().index(edgesIndex)
-    edgeTypes = sharedVariables.getArrayNames()[edgesIndex]
+    edgeTypes = sharedVariables['edges']
     
     for subgraphArray in subgraphArrayList:        
-        subgraphName = subgraphArray[subgraphNameIndex]
         nodes = subgraphArray[nodesIndex]
         nxSubgraph = nxGraph.subgraph(nodes)
-        nxSubgraph.graph['subgraphName'] = str(subgraphName)
+        nxSubgraph.graph = dict()
         nxSubgraph.graph['edgeTypes'] = ' '.join(edgeTypes)
         for edge in nxSubgraph.edges():
             edgeAttributes = nxSubgraph[edge[0]][edge[1]]
             assert 'type' in edgeAttributes
             edgeType = edgeAttributes['type']
-            if edgeTYpe not in edgeTypes:
-                nxSubgraph.remove_edge(edge)
+            if edgeType not in edgeTypes:
+                nxSubgraph.remove_edge(edge[0], edge[1])
         setNXAttributesFromArrayList(nxSubgraph.graph, subgraphArrayList,
-                                     subgraphArray, [nodesIndex, subgraphNameIndex])
+                                     subgraphArray, [nodesIndex])
         nxSubgraphs.append(nxSubgraph)
         
-def writeArrayListIntoNX(arrayList, nxGraph):
-    if arrayList.inheritsFrom('node'):
-        writeNodesIntoNX(arrayList, nxGraph)
-    elif arrayList.inheritsFrom('edge'):
-        writeEdgesIntoNX(arrayList, nxGraph)
-    elif arrayList.inheritsFrom('directedEdge'):
-        writeDirectedEdgesIntoNX(arrayList, nxGraph)
-    elif arrayList.inheritsFrom('subgraph'):
-        pass
-    else:
-        writeMetadataIntoNX(arrayList, nxGraph)
 
-# to be called after writeArrayListIntoNX (ie nxGraph already filled)
-def writeArrayListIntoNXSubgraphs(arrayList, nxGraph, nxSubgraphs):
-    if arrayList.inheritsFrom('subgraph'):
-        writeSubgraphsIntoNX(arrayList, nxGraph, nxSubgraphs)
-
-def isSQGDirected(sqg):
-    for arrayListType, arrayList in sqg.getArrayLists().items():
-        if arrayList.inheritsFrom('directedEdge'):
-            return True
-    return False
-        
-def isNXGraphDirected(nxGraph):
-    return type(nxGraph) == NX.DiGraph or type(nxGraph) == NX.MultiDiGraph    
             
             
--- tests/py/networkXTest.py
+++ tests/py/networkXTest.py
@@ -1,11 +1,23 @@
 import unittest
+import os
+from pymongo import Connection
+from pysqg.bioio import getTempFile
+from pysqg.py.networkX import networkxWrite, networkxRead
+from pysqg.arrayList import InMemoryArrayList
+import pysqg.tests.abstractTests as abstractTests
 
-class TestCase(unittest.TestCase):
-    def setup(self):
+class TestCase(abstractTests.AbstractTestCase):
+    def setUp(self):
+        """Constructs a simple graph to writes it into networkx then reads it back out.
+        """
+        abstractTests.AbstractTestCase.setUp(self)
+        sqg, graph = self.makeGraph(InMemoryArrayList)
+        nxGraph, nxSubgraphList = networkxWrite(sqg)    
+        self.graphs = [(networkxRead(nxGraph, nxSubgraphList), graph)]
+        
+    def tearDown(self):
         pass
     
-    def teardown(self):
-        pass
     
 def main():
     from pysqg.bioio import parseSuiteTestOptions
